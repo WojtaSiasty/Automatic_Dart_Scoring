9dbe543924ccb73794e17f18ccb936f633528d47
wojtasiasty <wojtasiasty@gmail.com>
2023-11-27 19:57:46 +0100

    Dziala UI

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from menu import Menu
 from camera import Camera
 
 def main():
-    cam1 = Camera(0)
+    # cam1 = Camera(0)
     # cam2 = Camera(1,'sectors1')
     # cam3 = Camera(2,'sectors2')
     menu = Menu()
 
     while menu.window_exist():
-        cam1.read_frame()
+        # cam1.read_frame()
         # cam2.read_frame()
         # cam3.read_frame()
 
-        cam1_hit1 = cam1.scores[0]
-        cam1_hit2 = cam1.scores[1]
-        cam1_hit3 = cam1.scores[2]
+        #cam1_hit1 = cam1.scores[0]
+        #cam1_hit2 = cam1.scores[1]
+        #cam1_hit3 = cam1.scores[2]
 
         # cam2_hit1 = cam2.scores[0]
         # cam2_hit2 = cam2.scores[1]
--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 class Menu():
 
@@ -8,31 +9,30 @@ class Menu():
         self.gamescore1 = 501
         self.gamescore2 = 501
 
-        self.hit1_value = 0
-        self.hit2_value = 0
-        self.hit3_value = 0
-
         self.window = tk.Tk()
         self.window.title("Dart")
         self.window.geometry("400x400")
-
+        
         label1 = ttk.Label(master = self.window, text = "Gracz 1")
-        score1 = ttk.Label(master = self.window, text = self.gamescore1)
+        self.score1 = ttk.Label(master = self.window, text = self.gamescore1)
         label2 = ttk.Label(master = self.window, text = "Gracz 2")
-        score2 = ttk.Label(master = self.window, text = self.gamescore2)
-        self.hit1 = ttk.Entry(master = self.window, textvariable = self.hit1_value)
-        self.hit2 = ttk.Entry(master = self.window, textvariable = self.hit2_value)
-        self.hit3 = ttk.Entry(master = self.window, textvariable = self.hit3_value)
+        self.score2 = ttk.Label(master = self.window, text = self.gamescore2)
+        self.hit1 = ttk.Entry(master = self.window)
+        self.hit2 = ttk.Entry(master = self.window)
+        self.hit3 = ttk.Entry(master = self.window)
         button = ttk.Button(master = self.window, text = "Następny Gracz", command = self.button_func)
 
+        self.hit1.insert(0,"0")
+        self.hit2.insert(0,"0")
+        self.hit3.insert(0,"0")
 
         self.window.columnconfigure((0,1,2), weight=1)
         self.window.rowconfigure((0,1,2,3,4,5,6), weight=1)
 
         label1.grid(row=0, column=0, sticky="s", pady=10)
-        score1.grid(row=1, column=0, sticky="n")
+        self.score1.grid(row=1, column=0, sticky="n")
         label2.grid(row=0, column=2, sticky="s", pady=10)
-        score2.grid(row=1, column=2, sticky="n")
+        self.score2.grid(row=1, column=2, sticky="n")
 
         self.hit1.grid(row=2, column=1)
         self.hit2.grid(row=3, column=1)
@@ -41,12 +41,43 @@ class Menu():
         button.grid(row=6, column=1, sticky="n")
 
     def button_func(self):
-        print("Button pressed")
-        # if turn == 1:
-        #     score = 
+        hit1_value = int(self.hit1.get())
+        hit2_value = int(self.hit2.get())
+        hit3_value = int(self.hit3.get())
+        score = hit1_value + hit2_value + hit3_value
 
-    # def get_window(self):
-    #     return self.window
+        if self.turn == 1:
+            if self.gamescore1 == score:
+                self.show_winner(1)
+
+            if score < self.gamescore1:
+                self.gamescore1 -= score
+                self.score1.config(text=self.gamescore1)
+
+            self.turn = 2
+        else:
+            if self.gamescore2 == score:
+                self.show_winner(2)
+
+            if score < self.gamescore2:
+                self.gamescore2 -= score
+                self.score2.config(text=self.gamescore2)
+
+            self.turn = 1
+
+        self.hit1.delete(0, tk.END)
+        self.hit2.delete(0, tk.END)
+        self.hit3.delete(0, tk.END)
+
+        self.hit1.insert(0,"0")
+        self.hit2.insert(0,"0")
+        self.hit3.insert(0,"0")
+
+
+    def show_winner(self, player):
+        self.window.destroy()
+        messagebox.showinfo("Koniec gry!", f"Gracz nr {player} wygrał!")
+
 
     def window_exist(self):
         try:
@@ -54,5 +85,3 @@ class Menu():
         except:
             return False
     
-    def destroy(self):
-        self.window.destroy()
